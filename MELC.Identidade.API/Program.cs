ea96e78d4763ce68dcd1e9175cd36bb5769556d0
Breno da Silva <breno.dasilvaa@gmail.com>
2024-02-06 22:01:07 -0300

    Atualiza identidade para usar postgres

--- MELC.Identidade.API/Program.cs
+++ MELC.Identidade.API/Program.cs
@@ -14,7 +14,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
 {
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
 builder.Services.AddDefaultIdentity<CustomUserIdentity>()
